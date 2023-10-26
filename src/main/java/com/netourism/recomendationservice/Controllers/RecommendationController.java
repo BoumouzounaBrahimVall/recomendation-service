d4bd66963b01f0cec59ad90e8c921cc204cfdf68
hcn31 <saidihocuine913@gmail.com>
2023-10-26 21:20:24 +0100

    endpoits:users of a hobby by id

--- src/main/java/com/netourism/recomendationservice/Controllers/RecommendationController.java
+++ src/main/java/com/netourism/recomendationservice/Controllers/RecommendationController.java
@@ -1,12 +1,14 @@
 package com.netourism.recomendationservice.Controllers;
 
 import com.netourism.recomendationservice.Models.Hobby;
+import com.netourism.recomendationservice.Models.User;
 import com.netourism.recomendationservice.Repositories.HobbyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -28,5 +30,34 @@ public class RecommendationController {
     hobbyRepository.deleteById(id);
     return new ResponseEntity<>(true, HttpStatus.OK);
 }
+
+@GetMapping("/{hobbyId}/users")
+public List<User> getUsersForHobby(@PathVariable(value="hobbyId") Long hobbyId) {
+    Hobby hobby = hobbyRepository.findById(hobbyId).orElse(null);
+    if (hobby != null) {
+        return hobby.getPracticers();
+    } else {
+            return Collections.emptyList(); // Or return an appropriate response.
+    }
+}
+
+
+@GetMapping("/users/{hobbyTitle}")
+public List<User> getUsersForHobby(@PathVariable("hobbyTitle") String title) {
+    Hobby hobby = hobbyRepository.findByTitle(title);
+    if (hobby != null){
+            return hobby.getPracticers();
+    } else {
+            return Collections.emptyList(); // Or return an appropriate response.
+    }
+    }
+
+
+
+    @GetMapping("/hobbies/{userId}")
+    public List<Hobby> getHobbiesForUser(@PathVariable Long userId) {
+        List<Hobby> hobbies = hobbyRepository.findHobbiesByUserId(userId);
+        return hobbies;
+    }
 
 }
--- src/main/java/com/netourism/recomendationservice/Repositories/HobbyRepository.java
+++ src/main/java/com/netourism/recomendationservice/Repositories/HobbyRepository.java
@@ -2,6 +2,12 @@ package com.netourism.recomendationservice.Repositories;
 
 import com.netourism.recomendationservice.Models.Hobby;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.List;
 
 public interface HobbyRepository extends Neo4jRepository<Hobby,Long> {
+    Hobby findByTitle(String title);
+    @Query("MATCH (u:User {userId: $userId})-[:practice]->(h:Hobby) RETURN h")
+    List<Hobby> findHobbiesByUserId(Long userId);
 }
